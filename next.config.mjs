e26acd3d99454687eff7882eb83b850e4328fdb7
boomchanotai <boom9387@hotmail.com>
2024-10-03 22:08:30 +0700

    fix: base path

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "export",
-  basePath: "/CUBS-website",
+  basePath: process.env.BasePath,
   images: {
     unoptimized: true,
   },
