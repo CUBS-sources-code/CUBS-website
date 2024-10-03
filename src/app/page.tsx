57c9bd277599a33b1ee40275da4684a108924d43
boomchanotai <boom9387@hotmail.com>
2024-10-03 18:56:43 +0700

    refactor: hero section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,16 +2,19 @@ import Banner from "@/components/Banner";
 import Blockathon from "@/components/Home/Blockathon";
 import BottomBanner from "@/components/Home/BottomBanner";
 import MainBackground from "@/components/Home/Background/MainBackground";
+import Hero from "@/components/Home/Hero";
 
 export default function Home() {
   return (
     <main className="relative min-h-screen overflow-hidden">
-      <div className="absolute inset-0 -z-10">
-        <MainBackground />
-      </div>
-      <Banner />
+      <Hero />
+      {/* <Banner />
       <Blockathon />
-      <BottomBanner />
+      <BottomBanner /> */}
+
+      {/* <div className="absolute inset-0 -z-10">
+        <MainBackground />
+      </div> */}
     </main>
   );
 }
