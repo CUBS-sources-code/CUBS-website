651ff7030d1aec6c6d90473c3f4e255b93c1cec8
agehcx <chega.malee@gmail.com>
2024-10-04 13:13:10 +0700

    fix: rm unused import

--- src/app/community/page.tsx
+++ src/app/community/page.tsx
@@ -2,13 +2,11 @@ import Benefits from "@/components/Community/Benefits";
 import CommunityHeader from "@/components/Community/CommunityHeader";
 import MemberBox from "@/components/Community/MemberBox";
 import OurMembers from "@/components/Community/OurMembers";
-import ReviewFromUs from "@/components/Community/ReviewFromUs";
 
 export default function CommunityPage() {
     return (
         <div>
             <CommunityHeader />
-            {/* <ReviewFromUs /> */}
             <Benefits />
             <OurMembers />
             <MemberBox/>
